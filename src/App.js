59c0844b2d3fe3ba748afc3844324b49c86af598
Love Kumar <lovekumar.200442@gmail.com>
2024-05-22 13:15:28 +0530

    catapi

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
 import './App.css';
 
+import { useEffect, useState } from 'react';
+
+import  Axios  from 'axios';
+
+
 function App() {
+  const[catFact,setCatFact]=useState("");
+
+  const getFact=()=>{
+    Axios.get("https://catfact.ninja/fact").then((res)=>{
+      setCatFact(res.data.fact)
+    })
+
+  }
+
+  useEffect(()=>{
+    getFact();
+
+
+  },[])
   return (
     <div className="App">
-      <h1>yo</h1>
-
+      <button onClick={getFact}>generate fact</button>
+      <p>{catFact}</p>
     </div>
   );
-}
+}  
 
 export default App;
